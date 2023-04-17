3a39d1b8423c6651c6fcc71b782a684d75ad5f45
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-17 12:46:28 +0200

    f

--- Pensumsjekk4/Bilforhandler.cs
+++ Pensumsjekk4/Bilforhandler.cs
@@ -4,11 +4,12 @@
     {
         public string Navn { get; private set; }
         public string Adresse { get; private set; }
-        public List<Bil> Biler { get; private set; }
+        public List<Bil> Biler { get; set; }
         public Bilforhandler(string navn, string adresse)
         {
             Navn = navn;
             Adresse = adresse;
+            Biler = new List<Bil>();
         }
         public void LeggTilBil(Bil bil)
         {
@@ -16,7 +17,7 @@
         }
         public void FjernBil(string regNummer)
         {
-            Biler.Remove(regNummer);
+            //Biler.Remove(regNummer);
         }
     }
 }
--- Pensumsjekk4/Program.cs
+++ Pensumsjekk4/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Sam!");
+            Bilforhandler bilforhandlerern = new Bilforhandler("PerPålolo", "Hell's fith circle");
+            Bil bil1 = new Bil("Mercedes", "YF66699", 2017, 0);
+            Bil bil2 = new Bil("Fiat", "KH69332", 2015, 12);
+            Bil bil3 = new Bil("Ford", "SD48278", 2009, 50000);
+            Bil bil4 = new Bil("Bugatti", "MM99909", 2012, 999999);
+            Bil bil5 = new Bil("BMW", "BM12345", 2023, 567);
+            bilforhandlerern.LeggTilBil(bil1);
+            bilforhandlerern.LeggTilBil(bil2);
+            bilforhandlerern.LeggTilBil(bil3);
+            bilforhandlerern.LeggTilBil(bil4);
+            bilforhandlerern.LeggTilBil(bil5);
+            foreach (Bil bil in bilforhandlerern.Biler)
+            {
+                Console.WriteLine(bil.Merke);
+            }
         }
     }
 }
